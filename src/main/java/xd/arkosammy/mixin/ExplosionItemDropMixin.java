e1ff768b7ea2b4c207e257d8fff8787cc7d1a90b
ArkoSammy12 <jamesvillarreal12@gmail.com>
2023-09-20 23:30:17 -0500

    Improve ExplosionItemDropMixin by using @ModifyReturnValue instead of a return callback

--- src/main/java/xd/arkosammy/mixin/ExplosionItemDropMixin.java
+++ src/main/java/xd/arkosammy/mixin/ExplosionItemDropMixin.java
@@ -1,26 +1,24 @@
 package xd.arkosammy.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.block.Block;
-import net.minecraft.entity.mob.CreeperEntity;
-import net.minecraft.world.explosion.Explosion;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xd.arkosammy.configuration.tables.PreferencesConfig;
 
 @Mixin(Block.class)
 public abstract class ExplosionItemDropMixin {
 
-    @Inject(method = "shouldDropItemsOnExplosion", at=@At("HEAD"), cancellable = true)
-    private void shouldDropItems(@NotNull Explosion explosion, CallbackInfoReturnable<Boolean> cir){
+    @ModifyReturnValue(method = "shouldDropItemsOnExplosion", at=@At("RETURN"))
+    private boolean shouldDropItems(boolean dropItems){
 
-        if(explosion.getCausingEntity() instanceof CreeperEntity && !PreferencesConfig.getDropItemsOnExplosions()){
+        if(!PreferencesConfig.getDropItemsOnExplosions()){
 
-            cir.setReturnValue(false);
+            return false;
 
         }
+
+        return dropItems;
 
     }
 
