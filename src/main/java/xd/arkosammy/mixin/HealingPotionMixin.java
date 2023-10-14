7687eea969d3dc9a4b8e470950f712ea6d79a085
ArkoSammy12 <jamesvillarreal12@gmail.com>
2023-10-14 18:54:21 -0500

    Finishing touches

--- src/main/java/xd/arkosammy/mixin/HealingPotionMixin.java
+++ src/main/java/xd/arkosammy/mixin/HealingPotionMixin.java
@@ -25,13 +25,9 @@ public abstract class HealingPotionMixin {
 
     @Inject(method = "onCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/thrown/PotionEntity;applySplashPotion(Ljava/util/List;Lnet/minecraft/entity/Entity;)V"), locals =  LocalCapture.CAPTURE_FAILSOFT)
     private void onSplashPotionHit(HitResult hitResult, CallbackInfo ci, ItemStack itemStack, Potion potion) {
-
         if(!PreferencesConfig.getHealOnHealingPotionSplash()) return;
-
         List<StatusEffect> statusEffects = potion.getEffects().stream().map(StatusEffectInstance::getEffectType).toList();
-
         if(statusEffects.contains(StatusEffects.INSTANT_HEALTH)) {
-
             BlockPos potionHitPosition;
             if (hitResult instanceof BlockHitResult blockHitResult) {
                 potionHitPosition = blockHitResult.getBlockPos().offset(blockHitResult.getSide());
@@ -40,21 +36,11 @@ public abstract class HealingPotionMixin {
             }
 
             ExplosionListHandler.getExplosionEventList().forEach(explosionEvent -> {
-
                 List<BlockPos> affectedBlockPositions = explosionEvent.getAffectedBlocksList().stream().map(AffectedBlock::getPos).toList();
-
                 if(affectedBlockPositions.contains(potionHitPosition)){
-
                     explosionEvent.setExplosionTimer(-1);
-
                 }
-
             });
-
         }
-
     }
-
-
-
 }
