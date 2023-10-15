d870ce8af5bffd8be3bd6453bf7d77f7bc532e39
ArkoSammy12 <jamesvillarreal12@gmail.com>
2023-10-14 20:54:22 -0500

    Ìmprove positionHitPosition detection

--- src/main/java/xd/arkosammy/mixin/HealingPotionMixin.java
+++ src/main/java/xd/arkosammy/mixin/HealingPotionMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.projectile.thrown.PotionEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
@@ -23,16 +24,20 @@ import java.util.List;
 @Mixin(PotionEntity.class)
 public abstract class HealingPotionMixin {
 
-    @Inject(method = "onCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/thrown/PotionEntity;applySplashPotion(Ljava/util/List;Lnet/minecraft/entity/Entity;)V"), locals =  LocalCapture.CAPTURE_FAILSOFT)
+    @Inject(method = "onCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/thrown/PotionEntity;applySplashPotion(Ljava/util/List;Lnet/minecraft/entity/Entity;)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void onSplashPotionHit(HitResult hitResult, CallbackInfo ci, ItemStack itemStack, Potion potion) {
         if(!PreferencesConfig.getHealOnHealingPotionSplash()) return;
         List<StatusEffect> statusEffects = potion.getEffects().stream().map(StatusEffectInstance::getEffectType).toList();
         if(statusEffects.contains(StatusEffects.INSTANT_HEALTH)) {
             BlockPos potionHitPosition;
-            if (hitResult instanceof BlockHitResult blockHitResult) {
+            if (hitResult.getType() == HitResult.Type.BLOCK) {
+                BlockHitResult blockHitResult = (BlockHitResult) hitResult;
                 potionHitPosition = blockHitResult.getBlockPos().offset(blockHitResult.getSide());
+            } else if(hitResult.getType() == HitResult.Type.ENTITY) {
+                EntityHitResult entityHitResult = (EntityHitResult) hitResult;
+                potionHitPosition = entityHitResult.getEntity().getBlockPos();
             } else {
-                potionHitPosition = new BlockPos((int) Math.round(hitResult.getPos().getX()), (int) Math.round(hitResult.getPos().getY()), (int) Math.round(hitResult.getPos().getZ()));
+                return;
             }
 
             ExplosionListHandler.getExplosionEventList().forEach(explosionEvent -> {
