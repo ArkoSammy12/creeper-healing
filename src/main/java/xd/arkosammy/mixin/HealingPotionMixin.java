d618a5f65b51786573e12fa6f93b1f20a1530ac6
ArkoSammy12 <jamesvillarreal12@gmail.com>
2023-10-15 21:38:32 -0500

    Use @Local annotation to capture local variable

--- src/main/java/xd/arkosammy/mixin/HealingPotionMixin.java
+++ src/main/java/xd/arkosammy/mixin/HealingPotionMixin.java
@@ -1,10 +1,10 @@
 package xd.arkosammy.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.projectile.thrown.PotionEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import xd.arkosammy.configuration.tables.PreferencesConfig;
 import xd.arkosammy.explosions.AffectedBlock;
 import xd.arkosammy.handlers.ExplosionListHandler;
@@ -24,8 +23,8 @@ import java.util.List;
 @Mixin(PotionEntity.class)
 public abstract class HealingPotionMixin {
 
-    @Inject(method = "onCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/thrown/PotionEntity;applySplashPotion(Ljava/util/List;Lnet/minecraft/entity/Entity;)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void onSplashPotionHit(HitResult hitResult, CallbackInfo ci, ItemStack itemStack, Potion potion) {
+    @Inject(method = "onCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/thrown/PotionEntity;applySplashPotion(Ljava/util/List;Lnet/minecraft/entity/Entity;)V"))
+    private void onSplashPotionHit(HitResult hitResult, CallbackInfo ci, @Local Potion potion) {
         if(!PreferencesConfig.getHealOnHealingPotionSplash()) return;
         List<StatusEffect> statusEffects = potion.getEffects().stream().map(StatusEffectInstance::getEffectType).toList();
         if(statusEffects.contains(StatusEffects.INSTANT_HEALTH)) {
