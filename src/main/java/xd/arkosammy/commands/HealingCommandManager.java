0f1a661db44c739fcd72554c643f1d008d3e52d8
ArkoSammy12 <jamesvillarreal12@gmail.com>
2023-10-24 21:05:09 -0500

    Polish whitelist feature. Renamed reload command to "reload_config" and move it outside the preferences sub-command category. Renamed "settings" sub-command category to "preferences"

--- src/main/java/xd/arkosammy/commands/HealingCommandManager.java
+++ src/main/java/xd/arkosammy/commands/HealingCommandManager.java
@@ -1,10 +1,17 @@
 package xd.arkosammy.commands;
 
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import xd.arkosammy.configuration.Config;
+
+import java.io.IOException;
 
 public final class HealingCommandManager {
 
@@ -15,12 +22,35 @@ public final class HealingCommandManager {
                 .literal("creeper-healing")
                 .requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(4))
                 .build();
+
+        //Reload Config node
+        LiteralCommandNode<ServerCommandSource> reloadNode = CommandManager
+                .literal("reload_config")
+                .requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(4))
+                .executes(context -> {
+
+                    try {
+                        HealingCommandManager.reload(context);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    return Command.SINGLE_SUCCESS;
+
+                })
+                .build();
+
         //Root connection
         dispatcher.getRoot().addChild(creeperHealingNode);
         ExplosionSourcesCommands.register(creeperHealingNode);
         ModeCommands.register(creeperHealingNode);
-        SettingsCommands.register(creeperHealingNode);
-        WhitelistCommands.register(creeperHealingNode);
+        PreferencesCommands.register(creeperHealingNode);
+    }
+
+    private static void reload(CommandContext<ServerCommandSource> ctx) throws IOException {
+        //If this returns true, then the config file exists, and we can update our values from it
+        if(Config.reloadConfigSettingsInMemory(ctx)) ctx.getSource().sendMessage(Text.literal("Config successfully reloaded"));
+        else ctx.getSource().sendMessage(Text.literal("Found no existing config file to reload values from").formatted(Formatting.RED));
     }
 
 }
