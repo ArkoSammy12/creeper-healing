f5fb4e8743eaaa4674b3f99b11135be55f2aeae7
ArkoSammy12 <jamesvillarreal12@gmail.com>
2023-11-01 10:41:59 -0500

    Undo unnecessary extra util method

--- src/main/java/xd/arkosammy/mixin/ExplosionListenerMixin.java
+++ src/main/java/xd/arkosammy/mixin/ExplosionListenerMixin.java
@@ -41,12 +41,11 @@ public abstract class ExplosionListenerMixin {
     @Shadow @Nullable public abstract LivingEntity getCausingEntity();
     @Shadow public abstract List<BlockPos> getAffectedBlocks();
     @Shadow @Final @Nullable private Entity entity;
-
     @Shadow public abstract DamageSource getDamageSource();
 
     @Inject(method = "collectBlocksAndDamageEntities", at = @At("RETURN"))
-    private void getExplodedBlocks(CallbackInfo ci){
-        if(canStoreExplosion(this.getCausingEntity(), this.entity, this.getDamageSource()))
+    private void storeCurrentExplosionIfNeeded(CallbackInfo ci){
+        if(shouldStoreExplosionFromSourceType(this.getCausingEntity(), this.entity, this.getDamageSource()))
             storeExplosion(this.getAffectedBlocks());
     }
 
@@ -71,7 +70,7 @@ public abstract class ExplosionListenerMixin {
     }
 
     @Unique
-    private boolean canStoreExplosion(LivingEntity causingLivingEntity, Entity causingEntity, DamageSource damageSource){
+    private boolean shouldStoreExplosionFromSourceType(LivingEntity causingLivingEntity, Entity causingEntity, DamageSource damageSource){
         return (causingLivingEntity instanceof CreeperEntity && ExplosionSourceConfig.getHealCreeperExplosions())
                 || (causingLivingEntity instanceof GhastEntity && ExplosionSourceConfig.getHealGhastExplosions())
                 || (causingLivingEntity instanceof WitherEntity && ExplosionSourceConfig.getHealWitherExplosions())
