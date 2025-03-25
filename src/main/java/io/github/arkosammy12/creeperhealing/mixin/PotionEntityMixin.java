b228c8db053c31169eabeaf97732aaee4b344469
ArkoSammy12 <jamesvillarreal12@gmail.com>
2025-03-25 12:36:29 -0500

    Update to 1.21.5

--- src/main/java/io/github/arkosammy12/creeperhealing/mixin/PotionEntityMixin.java
+++ src/main/java/io/github/arkosammy12/creeperhealing/mixin/PotionEntityMixin.java
@@ -1,16 +1,19 @@
 package io.github.arkosammy12.creeperhealing.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.component.ComponentType;
 import net.minecraft.component.type.PotionContentsComponent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.thrown.PotionEntity;
+import net.minecraft.entity.projectile.thrown.SplashPotionEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
-import net.minecraft.util.hit.HitResult;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import io.github.arkosammy12.creeperhealing.util.callbacks.SplashPotionCallbacks;
 
 @Mixin(PotionEntity.class)
@@ -21,10 +24,17 @@ public abstract class PotionEntityMixin extends ThrownItemEntity {
     }
 
     // Make explosions start healing when you throw a potion of healing or regeneration on them
-    @Inject(method = "onCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/thrown/PotionEntity;applySplashPotion(Lnet/minecraft/server/world/ServerWorld;Ljava/lang/Iterable;Lnet/minecraft/entity/Entity;)V"))
-    private void onSplashPotionHit(HitResult hitResult, CallbackInfo ci, @Local PotionContentsComponent potionContentsComponent) {
-        World world = this.getWorld();
-        SplashPotionCallbacks.ON_COLLISION.invoker().onPotionCollide(((PotionEntity) (Object) this), potionContentsComponent, hitResult, world);
+    @WrapOperation(method = "onBlockHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getOrDefault(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Ljava/lang/Object;"))
+    private Object onSplashPotionHit(ItemStack instance, ComponentType type, Object fallback, Operation<Object> original, @Local(argsOnly = true) BlockHitResult blockHitResult) {
+        Object result = original.call(instance, type, fallback);
+        if (!(((PotionEntity) (Object) this) instanceof SplashPotionEntity)) {
+            return result;
+        }
+        if (result instanceof PotionContentsComponent potionContentsComponent) {
+            World world = this.getWorld();
+            SplashPotionCallbacks.ON_COLLISION.invoker().onPotionCollide(((PotionEntity) (Object) this), potionContentsComponent, blockHitResult, world);
+        }
+        return result;
     }
 
 }
